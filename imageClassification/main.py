04e17d312e2ae38f9536389d8a3868e0c143e8fc
kantiny <56265288+kantiny@users.noreply.github.com>
2020-10-04 14:08:25 +0300

    Not shared with the test data

---
+++ imageClassification/main.py
@@ -0,0 +1,134 @@
+import tensorflow as tf
+from tensorflow import keras
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+train_dir = os.path.join('C:/Users/Аня/Desktop/Тестовое задание/ImageClassification/data/', 'IDRND_FASDB_train')
+test_dir = os.path.join('C:/Users/Аня/Desktop/Тестовое задание/ImageClassification/data/', 'IDRND_FASDB_train')
+
+train_real_dir = os.path.join(train_dir, 'real')  # директория с картинками оригинала для обучения
+train_real_dir = os.path.join(train_dir, 'spoof')  # директория с картинками копий для обучения
+
+test_real_dir = os.path.join(train_dir, 'real')  # директория с картинками оригинала для тестирования
+test_spoof_dir = os.path.join(train_dir, 'spoof')  # директория с картинками копий для тестирования
+
+total_train = len(os.listdir(train_real_dir)) + len(os.listdir(train_real_dir))
+total_val = len(os.listdir(test_real_dir)) + len(os.listdir(test_spoof_dir))
+
+batch_size = 128
+epochs = 15
+IMG_HEIGHT = 150
+IMG_WIDTH = 150
+
+train_image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255) # Генератор для тренировочных данных
+test_image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255) # Генератор для тестовых данных
+
+# Training
+train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
+                                                           directory=train_dir,
+                                                           shuffle=True,
+                                                           target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                           class_mode='binary')
+
+# Testing
+val_data_gen = test_image_generator.flow_from_directory(batch_size=batch_size,
+                                                              directory=test_dir,
+                                                              target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                              class_mode='binary')
+
+
+# Neural network
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH ,3)),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['acc'])
+
+# Training
+history = model.fit_generator(
+    train_data_gen,
+    steps_per_epoch=total_train // batch_size,
+    epochs=epochs,
+    validation_data=val_data_gen,
+    validation_steps=total_val // batch_size
+)
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+'''
+
+validation_image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255) # Генератор для проверочных данных
+
+# Testing
+val_data_gen = validation_image_generator.flow_from_directory(batch_size=batch_size,
+                                                              directory=validation_dir,
+                                                              target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                              class_mode='binary')
+# Neural network
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH ,3)),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['acc'])
+
+# model.summary() Показать все слои модели
+
+# Training
+history = model.fit_generator(
+    train_data_gen,
+    steps_per_epoch=total_train // batch_size,
+    epochs=epochs,
+    validation_data=val_data_gen,
+    validation_steps=total_val // batch_size
+)
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+'''
