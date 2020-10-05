797db10f3f19f3a34f61e47f29daf534695ed2d7
kantiny <56265288+kantiny@users.noreply.github.com>
2020-10-05 14:49:07 +0300

    model plus process of testing another variant (not work)

--- imageClassification/main.py
+++ imageClassification/main.py
@@ -5,6 +5,7 @@ from matplotlib import image
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import skimage
 
 batch_size = 128
 epochs = 15
@@ -26,7 +27,6 @@ model = tf.keras.models.Sequential([
 dirname = 'C:/Users/Аня/Desktop/Тестовое задание/ImageClassification/data/'
 train_dir = os.path.join(dirname, 'IDRND_FASDB_train')
 test_dir = os.path.join(dirname, 'test')
-print(test_dir == 'C:/Users/Аня/Desktop/Тестовое задание/ImageClassification/data/test')
 
 
 def train_predict():
@@ -41,8 +41,6 @@ def train_predict():
 
     train_image_generator = tf.keras.preprocessing.image.ImageDataGenerator(
         rescale=1. / 255)  # Генератор для тренировочных данных
-    test_image_generator = tf.keras.preprocessing.image.ImageDataGenerator(
-        rescale=1. / 255)  # Генератор для тестовых данных
 
     # Training
     train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
@@ -50,12 +48,6 @@ def train_predict():
                                                                shuffle=True,
                                                                target_size=(IMG_HEIGHT, IMG_WIDTH),
                                                                class_mode='binary')
-
-    # Testing
-    val_data_gen = test_image_generator.flow_from_directory(batch_size=batch_size,
-                                                            directory=test_dir,
-                                                            target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                                            class_mode='binary')
 
     model.compile(optimizer='adam',
                   loss='binary_crossentropy',
@@ -72,22 +64,17 @@ def train_predict():
     model.save_weights("my_model_weights.h5")
 
     acc = history.history['acc']
-    # val_acc = history.history['val_acc']
-
     loss = history.history['loss']
-    # val_loss = history.history['val_loss']
-
-    epochs_range = range(epochs)
 
     plt.figure(figsize=(8, 8))
     plt.subplot(1, 2, 1)
-    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(range(epochs), acc, label='Training Accuracy')
     # plt.plot(epochs_range, val_acc, label='Validation Accuracy')
     plt.legend(loc='lower right')
     plt.title('Training and Validation Accuracy')
 
     plt.subplot(1, 2, 2)
-    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(range(epochs), loss, label='Training Loss')
     # plt.plot(epochs_range, val_loss, label='Validation Loss')
     plt.legend(loc='upper right')
     plt.title('Training and Validation Loss')
@@ -97,31 +84,29 @@ def train_predict():
 # train_predict()
 
 model.load_weights('my_model_weights.h5')
+
+'''
 train_image_generator = tf.keras.preprocessing.image.ImageDataGenerator(
     rescale=1. / 255)  # Генератор для тренировочных данных
+
 test_image_generator = tf.keras.preprocessing.image.ImageDataGenerator(
     rescale=1. / 255)  # Генератор для тестовых данных
+
 train_data_gen = train_image_generator.flow_from_directory(batch_size=batch_size,
-                                                           directory=train_dir,
-                                                           shuffle=True,
-                                                           target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                                           class_mode='binary')
+                                                            directory=train_dir,
+                                                            shuffle=True,
+                                                            target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                            class_mode='binary')
 test_data_gen = test_image_generator.flow_from_directory(batch_size=batch_size,
                                                          directory=test_dir,
                                                          shuffle=False,
                                                          target_size=(IMG_HEIGHT, IMG_WIDTH),
-                                                         class_mode='categorical')
-STEP_SIZE_TEST = test_data_gen.n // test_data_gen.batch_size
-print(STEP_SIZE_TEST)
-test_data_gen.reset()
-pred = model.predict_generator(test_data_gen, steps=STEP_SIZE_TEST, verbose=1)
-print(pred)
-predicted_class_indices = np.argmax(pred, axis=1)
-labels = (train_data_gen.class_indices)
-labels = dict((v, k) for k, v in labels.items())
-predictions = [labels[k] for k in predicted_class_indices]
-'''for file in os.listdir(test_dir):
-    data = image.imread(os.path.join(test_dir, file))
-    print(data.shape)
-    reshaped_image = data.reshape((IMG_WIDTH, IMG_HEIGHT))
-    output = model.predict(reshaped_image)'''
+                                                         class_mode=None)
+
+for pic in test_data_gen:
+    predictions = model.predict(pic)
+    print(predictions)
+'''
+images = skimage.io.ImageCollection(test_dir)
+for ime in images:
+    model.predict(ime.shape())
